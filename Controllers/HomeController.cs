5cc78db11a41d4ec9f55a4ee744ad2e9aeadb7c4
Scott Sauber <stsauber@gmail.com>
2020-01-06 22:50:51 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string post)
+        {
+            var files = Request.Files;
+            foreach (string fileName in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileName];
+            }
+
+            return View();
+        }
+    }
+}
